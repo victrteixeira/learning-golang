4e7ff02b55ffb47a4415bb656e861986c3400aea
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-03-19 17:05:19 -0300

    test alias

--- go-fullcourse/exercises/slices.go
+++ go-fullcourse/exercises/slices.go
@@ -1,5 +1,7 @@
 package exercises
 
+import "fmt"
+
 func Slice_Declaration() (string) {
-	
+	a := []int {1,2,3}
 }
--- go-fullcourse/main.go
+++ go-fullcourse/main.go
@@ -6,15 +6,6 @@ import (
 )
 
 func main() {
-	//res := exercises.Array_Example1()
-	//fmt.Print(res)
-
-	//res2 := exercises.Array_SimpleDeclaration()
-	//fmt.Print(res2)
-
-	//res3 := exercises.Array_MatrixDeclaration()
-	//fmt.Print(res3)
-
-	res4 := exercises.Array_AsValues()
-	fmt.Print(res4)
+	res := exercises.Slice_Declaration()
+	fmt.Print(res)
 }
