ce975f9dd37da21ab90ef374762d037b1144ef6e
Victor Teixeira <vicktorteixeira.7@gmail.com>
2023-03-19 15:39:09 -0300

    more arrays examples

--- go-fullcourse/exercises/arraysandslices.go
+++ go-fullcourse/exercises/arraysandslices.go
@@ -2,7 +2,36 @@ package exercises
 
 import "fmt"
 
-func Array_Example1() string {
+func Array_SimpleDeclaration() string {
 	grades := [3]int{32, 21, 42} // declaration
 	return fmt.Sprintf("Grades: %v\n", grades)
+}
+
+func Array_ManagingSize() string {
+	var students [3]string
+	fmt.Printf("Students: %v\n", students)
+	students[0] = "Victor"
+	students[1] = "Lisa"
+	students[2] = "Arnold"
+	fmt.Printf("Student #1: %v\n", students[0])
+	return fmt.Sprintf("Number of Students: %v\n", len(students))
+}
+
+func Array_MatrixDeclaration() string {
+	var matrix [3][3]int = [3][3]int{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
+	fmt.Printf("Matrix here we go:\n%v\n", matrix)
+
+	var matrixAlternative [3][3]int
+	matrixAlternative[0] = [3]int{1, 0, 0}
+	matrixAlternative[1] = [3]int{0, 1, 0}
+	matrixAlternative[2] = [3]int{0, 0, 1}
+	return fmt.Sprintln(matrixAlternative)
+}
+
+func Array_AsValues() string {
+	a := [...]int{1, 2, 3}
+	b := &a // Without pointer, Go will copy the entire array over, what could let the program slow.
+	b[1] = 5
+	res := fmt.Sprintln(a) + fmt.Sprintln(b)
+	return res
 }
--- go-fullcourse/main.go
+++ go-fullcourse/main.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-	res := exercises.Array_Example1()
-	fmt.Print(res)
+	//res := exercises.Array_Example1()
+	//fmt.Print(res)
+
+	//res2 := exercises.Array_SimpleDeclaration()
+	//fmt.Print(res2)
+
+	//res3 := exercises.Array_MatrixDeclaration()
+	//fmt.Print(res3)
+
+	res4 := exercises.Array_AsValues()
+	fmt.Print(res4)
 }
